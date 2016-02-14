5c48d5ff626eef27cb8abfeed3ade8965e5d9532
p7157568 <p7157568@gmail.com>
2016-02-14 16:32:51 +0700

    Initial commit

---
+++ app/src/main/java/dhbk/android/tabswipeview/Tab2.java
@@ -0,0 +1,27 @@
+package dhbk.android.tabswipeview;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tab2 extends Fragment {
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    public static Tab2 newInstance(int sectionNumber) {
+        Tab2 fragment = new Tab2();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.tab2, container, false);
+        return v;
+    }
+}
+
